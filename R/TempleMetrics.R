448c4d86f7371c2dc8586d973edb52b276ddda1b
Brantly Callaway <brant.callaway@gmail.com>
2017-08-29 14:49:36 -0400

    added dr code

--- R/TempleMetrics.R
+++ R/TempleMetrics.R
@@ -1,16 +1,12 @@
-#' @title helloWorld
-#'
-#' @description print Hello World
-#'
-#' @param other an alternative string to print
-#'
-#' @return NULL
-#'
-#' @export
-helloWorld <- function(other=NULL) {
-    if (is.null(other)) {
-        print("Hello World")
-    } else {
-        print(other)
-    }
+dr <- function(y, x, data, yname, xnames) {
+    ##form <- as.formula(formla)
+    ##dta <- model.frame(terms(formla,data=data),data=data) #or model.matrix
+    IY <- 1*(data[,yname] <= y)
+    X <- data[,xnames]
+    dta <- cbind.data.frame(IY, X)
+    x <- data.frame(t(x))
+    colnames(x) <- xnames
+    formla <- as.formula(paste0("IY ~", paste(xnames, collapse="+")))
+    lgit <- glm(formla, data=dta, family=binomial(link=logit))
+    predict(lgit, newdata=x, type="response")
 }
