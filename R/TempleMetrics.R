a4be73cc40dd540ab6b2e4fb4286614365055984
Brantly Callaway <brant.callaway@gmail.com>
2017-08-29 16:10:29 -0400

    updated dr code

--- R/TempleMetrics.R
+++ R/TempleMetrics.R
@@ -1,12 +1,44 @@
 dr <- function(y, x, data, yname, xnames) {
     ##form <- as.formula(formla)
     ##dta <- model.frame(terms(formla,data=data),data=data) #or model.matrix
+    x <- data.frame(t(x))
+    colnames(x) <- xnames
+    lgit <- drs.inner(y, data, yname, xnames)
+    predict(lgit, newdata=x, type="response")
+}
+
+drs <- function(yvals, data, yname, xnames) {
+    lapply(yvals, drs.inner, data=data, yname=yname, xnames=xnames)
+}
+
+drs.inner <- function(y, data, yname, xnames) {
     IY <- 1*(data[,yname] <= y)
     X <- data[,xnames]
     dta <- cbind.data.frame(IY, X)
-    x <- data.frame(t(x))
-    colnames(x) <- xnames
     formla <- as.formula(paste0("IY ~", paste(xnames, collapse="+")))
     lgit <- glm(formla, data=dta, family=binomial(link=logit))
-    predict(lgit, newdata=x, type="response")
+    lgit
+}
+
+distreg <- function(yvals, data, yname, xnames) {
+    DR(yvals, drs(yvals, data, yname, xnames))
+}
+
+## DR class
+DR<- function(yvals, glmlist) {
+    out <- list(yvals=yvals, glmlist=glmlist)
+    class(out) <- "DR"
+    out
+}
+
+predict.DR <- function(drobj, y, x) {
+    yvals <- drobj$yvals
+    glmlist <- drobj$glmlist
+    if (! (y %in% yvals)) {
+        stop("must provide value of y in drobj$yvals")
+    }
+    x <- data.frame(t(x))
+    colnames(x) <-  names(coef(glmlist[[1]]))[-1]
+    i <- which(yvals==y)[1]
+    predict(glmlist[[i]], newdata=x, type="response")
 }
