874ebece2217690fead81578ff41727905468018
WeigeHuangEcon <tuf68739@temple.edu>
2018-08-20 19:07:27 -0400

    llscm

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,6 @@
+#TempleMetrics 1.2.0
+  * New code for local linear smooth varying coefficients models
+
 #TempleMetrics 1.1.0
   * Added code for computing conditional distributions using first step quantile regression
   * Added code for computing conditional distributions using first step local linear distribution regression
--- R/llscm.R
+++ R/llscm.R
@@ -1,27 +1,65 @@
-#' @title llscm
+#' @title llscm.inner
 #'
-#' @description local linear estimator of smoothing coefficient model
+#' @description internal function that does the heavy lifting
 #'
 #' @param t conditional at a value T=t
 #' @param Y outcome variable
 #' @param T treatment variable
 #' @param Xmat covariates
 #' @param h bandwidth
-#'
-#' @return Coefficients
+#' @keywords internal
+#' @return a 2*k (k being the dimension of X) vector of coefficients, the first
+#'  k are the "levels", the second k are the derivatives with respect to each
+#'  element of X.
 #'
 #' @export
-
-## local linear smooth varying coefficient model
-llscm <- function(t, Y, T, Xmat, h) {
+#'
+#'
+llscm.inner <- function(t, Y, T, Xmat, h) {
     X <- cbind(Xmat, (T - t)*Xmat)
     y <- as.matrix(Y)
     n <- length(T)
     if (is.null(h)) {
         h <- 1.06*sd(T)*n^(-1/5) ## check that this is right
     }
-    K <- diag(TempleMetrics::k(T-t, h=h, type="gaussian"))
+    K <- diag(k(T-t, h=h, type="gaussian"))
     Coefficients <- solve(t(X)%*%K%*%X)%*%t(X)%*%K%*%y
     Coefficients
 }
+
+
+#' @title llscm
+#'
+#' @description local linear estimator of smoothing coefficient model
+#' @param formla a formula y ~ treatment
+#' @param xformla one sided formula for x variables to include, e.g. ~x1 + x2
+#' @param data the data.frame where y, t, and x are
+#' @inheritParams llscm.inner
+#' @return a 2*k (k being the dimension of X) vector of coefficients, the first
+#'  k are the "levels", the second k are the derivatives with respect to each
+#'  element of X.
+#' @examples
+#' data(igm)
+#' igm$hs=ifelse(igm$HEDUC=="HS",1,0)
+#' igm$col=ifelse(igm$HEDUC=="COL",1,0)
+#' formla=lcfincome~lfincome
+#' xformla=~hs+col
+#' t=mean(igm$lfincome)
+#' h=1.2
+#' data=igm
+#' llscm(formla,xformla,data,t,h)
+#' @export
+llscm<-function(formla,xformla,data,t,h){
+  formla=as.formula(formla)
+  xformla=as.formula(xformla)
+  YT=model.frame(terms(formla,data=data),data=data)
+  X=model.frame(terms(xformla,data=data),data=data)
+  Y=YT[,1]
+  T=YT[,2]
+  Xmat=as.matrix(X)
+  out<-llscm.inner(t=t, Y=Y, T=T, Xmat=Xmat, h=h)
+  out
+}
+
+
 
